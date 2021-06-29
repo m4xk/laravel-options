e53dcbb2f75a075f161961324128c95aad49c9cd
m4xk <maxx.kovalev@gmail.com>
2021-06-29 19:38:25 +0300

    feat: Added scopes

--- src/Option.php
+++ src/Option.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
+
+    const SCOPE_DEFAULT = 'default';
+
     /**
      * Indicates if the model should be timestamped.
      *
@@ -30,61 +33,92 @@ class Option extends Model
     protected $fillable = [
         'key',
         'value',
+        'scope'
     ];
 
     /**
      * Determine if the given option value exists.
      *
-     * @param  string  $key
+     * @param string $key
+     * @param string $scope
      * @return bool
      */
-    public function exists($key)
+    public function exists($key, $scope = self::SCOPE_DEFAULT)
     {
-        return self::where('key', $key)->exists();
+        return self::where('key', $this->buildKey($key, $scope))->exists();
     }
 
     /**
      * Get the specified option value.
      *
-     * @param  string  $key
-     * @param  mixed   $default
+     * @param string $key
+     * @param string $scope
+     * @param mixed $default
      * @return mixed
      */
-    public function get($key, $default = null)
+    function get($key, $default = null, $scope = self::SCOPE_DEFAULT)
     {
-        if ($option = self::where('key', $key)->first()) {
-            return $option->value;
+        if ($key === '*') {
+            return self::where('key', 'LIKE', "$scope.%")->get()->mapWithKeys(
+                function ($item) use ($scope) {
+                    return [preg_replace("/^$scope./", '', $item['key']) => unserialize($item['value'])];
+                }
+            )->toArray();
+        }
+
+        if ($option = Option::where('key', $this->buildKey($key, $scope))->first()) {
+            return unserialize($option->value);
         }
 
         return $default;
     }
 
+
     /**
      * Set a given option value.
      *
-     * @param  array|string  $key
-     * @param  mixed   $value
+     * @param array|string $key
+     * @param mixed $value
+     * @param string $scope
      * @return void
      */
-    public function set($key, $value = null)
+    function set($key, $value = null, $scope = self::SCOPE_DEFAULT)
     {
-        $keys = is_array($key) ? $key : [$key => $value];
-
-        foreach ($keys as $key => $value) {
-            self::updateOrCreate(['key' => $key], ['value' => $value]);
+        if (is_array($key) && func_num_args() == 2) {
+            foreach ($key as $keyName => $keyVal) {
+                Option::updateOrCreate(
+                    ['key' => $this->buildKey($keyName, $scope = $value)],
+                    ['value' => serialize($keyVal)]
+                );
+            }
+        } else {
+            Option::updateOrCreate(['key' => $this->buildKey($key, $scope)], ['value' => serialize($value)]);
         }
-
-        // @todo: return the option
     }
 
     /**
      * Remove/delete the specified option value.
      *
-     * @param  string  $key
+     * @param string $key
+     * @param string $scope
      * @return bool
      */
-    public function remove($key)
-    {
-        return (bool) self::where('key', $key)->delete();
+    public function remove(
+        $key,
+        $scope = self::SCOPE_DEFAULT
+    ) {
+        return (bool)self::where('key', $this->buildKey($key, $scope))->delete();
     }
+
+
+    /**
+     * @param $key
+     * @param string $scope
+     * @return string
+     */
+    protected function buildKey($key, $scope = self::SCOPE_DEFAULT)
+    {
+        return $scope . '.' . $key;
+    }
+
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,37 +1,41 @@
 <?php
 
-if (! function_exists('option')) {
+use Appstract\Options\Option;
+
+if (!function_exists('option')) {
     /**
      * Get / set the specified option value.
      *
      * If an array is passed as the key, we will assume you want to set an array of values.
      *
-     * @param  array|string  $key
-     * @param  mixed  $default
+     * @param array|string $key
+     * @param mixed $default
+     * @param string $scope
      * @return mixed|\Appstract\Options\Option
      */
-    function option($key = null, $default = null)
+    function option($key = null, $default = null, $scope = Option::SCOPE_DEFAULT)
     {
         if (is_null($key)) {
             return app('option');
         }
 
         if (is_array($key)) {
-            return app('option')->set($key);
+            return app('option')->set($key, $scope = $scope);
         }
 
         return app('option')->get($key, $default);
     }
 }
 
-if (! function_exists('option_exists')) {
+if (!function_exists('option_exists')) {
     /**
      * Check the specified option exits.
      *
-     * @param  string  $key
+     * @param string $key
+     * @param string $scope
      * @return mixed
      */
-    function option_exists($key)
+    function option_exists($key, $scope = Option::SCOPE_DEFAULT)
     {
         return app('option')->exists($key);
     }
